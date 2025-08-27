54d54495bb95fef5f713832c3c56d01ae3a288d6
ChousX <gmanenergy@gmail.com>
2025-08-27 12:33:40 -0400

    started work on bevy snippets

--- lua/snippets/rust.lua
+++ lua/snippets/rust.lua
@@ -11,5 +11,37 @@ local i = ls.insert_node
 local f = ls.function_node
 local c = ls.choice_node
 local fmt = require("luasnip.extras.fmt").fmt
+local rep = require("luasnip.extras").rep
 
-return {}
+return {
+	-- The s() function defines the snippet
+	s({
+		trig = "hi",
+		dscr = "A simple 'Hello, world!' example",
+	}, {
+		t("print('Hello, world!')"),
+	}),
+
+	-- Bevy Plugin
+	s(
+		"plugin",
+		fmt(
+			[[
+
+        pub struct {}Plugin;
+
+        impl Plugin for {}Plugin {{
+            fn build(&self, app: &mut App) {{
+                app
+                    {};
+            }}
+        }}
+    ]],
+			{
+				i(1, "My"),
+				rep(1),
+				i(0, "// .add_systems(...)"),
+			}
+		)
+	),
+}
